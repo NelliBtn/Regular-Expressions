f3caf7bf80795e98bd7a69315b6f02664610e91a
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-29 15:40:57 -0500

    theory

---
+++ regular-expressions.js
@@ -0,0 +1,7 @@
+/**
+ * Regular expressions are a way to describe patterns in string data.
+ * They form a small separate language that is part of JS and many other languages and systems.
+ * 
+ * The regular expression is a type of object. 
+ * It can be constructed with the RegExp constructor or written as a literal value by enclosing a pattern in /'s.
+ */
