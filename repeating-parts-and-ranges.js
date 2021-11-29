9972dac37d1bdb868e32538afd6a4ce50fd1377c
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-29 18:00:30 -0500

    ranges and open-ended ranges

--- repeating-parts-and-ranges.js
+++ repeating-parts-and-ranges.js
@@ -14,3 +14,15 @@ console.log(/'\d+'/.test("''")); //false
 console.log(/'\d*'/.test("'123'")); //true
 console.log(/'\d*'/.test("''")); //true
 
+/*
+ RANGES {}
+ PATTERN SHOULD OCCUR A PRECISE NUMBER OF TIMES
+*/
+
+let dateTime = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
+console.log(dateTime.test('1-30-2003 8:45')); //true
+
+// to specify OPEN-ENDED RANGES omit number after comma => {5,}
+
+console.log(/\d{3,}/.test('22222')); //true
+console.log(/\d{3,}/.test('22')); //false
