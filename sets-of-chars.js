d7a5506a004347665d28cd089c0f7f64d5beea7b
Nelli Butenko <butenkonelly93@gmail.com>
2021-11-29 16:43:08 -0500

    shortcuts for common char groups

--- sets-of-chars.js
+++ sets-of-chars.js
@@ -6,3 +6,15 @@
 // match all strings that contain a digit
 console.log(/[0123456789]/.test('in 1992')); //true
 console.log(/[0-9]/.test('in 1992')); //true
+
+// BUILT-IN SHORTCUTS for common character groups:
+
+/**
+ \d - any digit
+ \w - an alphanumeric character (all letters and digits)
+ \s - any whitespace character (space, tab, newline, etc)
+ \D - NOT a digit
+ \W - NONalpanumeric
+ \S - NONwhitespace
+ . - any char except for a newline
+ */
